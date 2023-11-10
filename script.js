ef563d0536606c1706e15a36030b0fbdde8e268a
likago <146434090+likago@users.noreply.github.com>
2023-11-10 18:32:24 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+let userName="lika";
+let age=27;
+const birthYear=1996;
+let isMarried=false;
+console.log(userName);
+let person= null;
+person= {
+    userName: "lika",
+    age: 27,
+    isMarried:false,
+};
+console.log("my name is "+person.userName);
+console.log(`I am ${person.age} years old`);
+let x=42;
+let y=13;
+let sum=x+y;
+console.log(sum);
+var lenght=7;
+var width=5;
+var area= lenght * width;
+console.log(area);
